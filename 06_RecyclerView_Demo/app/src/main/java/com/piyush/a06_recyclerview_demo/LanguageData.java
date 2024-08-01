ccb924a978aadea8d0f80e9d64871692bac35e2d
Piyush014 <109540904+Piyush014@users.noreply.github.com>
2024-08-01 22:49:38 +0530

    Added the Recycler View Demo Application

---
+++ 06_RecyclerView_Demo/app/src/main/java/com/piyush/a06_recyclerview_demo/LanguageData.java
@@ -0,0 +1,19 @@
+package com.piyush.a06_recyclerview_demo;
+
+public class LanguageData {
+    private String title;
+    private int logo;
+
+    public LanguageData(String title, int logo) {
+        this.title = title;
+        this.logo = logo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLogo() {
+        return logo;
+    }
+}
---
+++ app/src/main/java/com/piyush/a05_view_pager_demo/FragmentPageAdapter.kt
@@ -0,0 +1,4 @@
+package com.piyush.a05_view_pager_demo
+
+class FragmentPageAdapter {
+}
